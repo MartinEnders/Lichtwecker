9cb705ada17eacacc9fe07026a91ab7597161c32
Martin R. Enders <martin@martin-enders.de>
2018-01-06 13:20:27 +0100

    Anpassung von Wlan-Script. Auf der neu installierten version von Raspian ist der output von ifconfig anders.

--- wlan-helper-script/wlan.sh
+++ wlan-helper-script/wlan.sh
@@ -2,7 +2,7 @@
 # check if a wlan0 if exists
 if echo `/sbin/ifconfig` | grep -q $1; then
 #check if there is IP Address
-  if echo `/sbin/ifconfig $1` | grep -q "inet addr"; then
+  if echo `/sbin/ifconfig $1` | grep -q "inet "; then
     exit 0
   fi
   /sbin/modprobe -r 8192cu
